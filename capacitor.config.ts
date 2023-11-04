ef1cf9c130801fcd5199069d4f23806324e9e0b6
Fernando Rojas <frojas@monchis.com.py>
2023-11-04 19:48:12 +0000

    fix: launcher icon and splash

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,8 +1,18 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
+  plugins: {
+    SplashScreen: {
+      androidSplashResourceName: "splash",
+      androidScaleType: "CENTER_CROP",
+      splashFullScreen: true,
+      splashImmersive: true,
+      layoutName: "launch_screen",
+      useDialog: true,
+    }
+  },
   appId: 'io.ionic.starter',
-  appName: 'qrcreator',
+  appName: 'QR Creador',
   webDir: 'www',
   server: {
     androidScheme: 'https'
